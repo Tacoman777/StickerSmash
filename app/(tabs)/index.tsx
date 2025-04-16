03b9ded73057c40bbde719262700ee50e8aa0280
Tacoman777 <166954649+Tacoman777@users.noreply.github.com>
2025-04-15 20:33:11 -0400

    Page Navigation
    - nav bar at bottom of screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,15 @@
 //https://docs.expo.dev/tutorial/create-your-first-app/
 
 import { Text, View,  StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -19,6 +23,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
     color: '#fff',
   },
 });
